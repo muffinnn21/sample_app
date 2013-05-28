834b2131d922262de85865f366d7c24e31e1ee48
Dany Voisine <muffinnn21@gmail.com>
2013-05-28 17:39:41 -0400

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,17 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
+describe "Contact" do
 
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
   describe "Help page" do
     it "should have the content 'Help page'" do
