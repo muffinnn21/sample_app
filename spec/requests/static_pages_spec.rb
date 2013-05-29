52275fd2269a89a0c044942bf3c6d8d5d7239008
Dany Voisine <muffinnn21@gmail.com>
2013-05-28 21:35:00 -0400

    added app_helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,26 @@ require 'spec_helper'
 describe "Static pages" do
 let(:titre) {"Ruby on Rails Tutorial Sample App"}
 
-  describe "Home page" do
+
+
+describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
+
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{titre} | Home")
+      expect(page).to have_title("#{titre}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
+
 describe "Contact" do
 
     it "should have the content 'Contact'" do
